d228a2a8c12698533da5722da6b63b8105674cf5
muffin <squishysparrow7.7@gmail.com>
2021-08-10 19:21:18 +1200

    Simple file containing rgb constants

---
+++ colors.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# colors.py
+# A bunch of color constants for importing
+
+class RGB:
+    WHITE = (255, 255, 255)
+
+
+if __name__ == "__main__":
+    # Print defined colours
+
+    print("  -- RGB --  ")
+    for rgb in dir(RGB):
+        # Avoid dunder methods
+        if rgb[:2] != "__":
+            # Eval is often frowned upon but there is no danger here
+            # because it is only calling the attributes from the class
+            print("{}\t{}".format(rgb, eval("RGB." + rgb)))
+
+
