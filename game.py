ba68ff243dc82af5d690931856eb1d8d7f85bc7a
muffin <squishysparrow7.7@gmail.com>
2021-08-12 22:23:36 +1200

    Menu class now displays text

--- game.py
+++ game.py
@@ -29,20 +29,63 @@ class Menu:
     """
     This class lets you create an interactive menu that can be "blited" into a surface
     """
-    def __init__(self, options, size):
+    def __init__(self, options, size, font: pygame.font.Font,
+                 antialias=True,
+                 text_color=colors.RGB.BLACK, background_color=colors.RGBA.TRANSPARENT,
+                 padding=0):
         """
         :param options: A list of strings that will be the options in the menu
         :param size: Tuple of size in px
+        :param font: Pygame font object
+
+        :keyword antialias: Bool. True
+        :keyword text_color: RGB(A). Black
+        :keyword padding: In pixels, distance from each side of the surface that will not be touched.
+                          Can be either an int to signify the same padding on all sides or a tuple
+                          of length 4 if you want to be specific. Clockwise.
         """
+        # Parameters
         self.options = options
         self.size = size
+        self.font = font
+
+        # Keywords
+        self.antialias = antialias
+        self.text_color = text_color
+        self.background_color = background_color
+
+        # Validate padding
+        if isinstance(padding, int):
+            self.padding = (padding, padding, padding, padding)
+
+        elif isinstance(padding, tuple) and len(padding) == 4:
+            self.padding = padding
+
+        else:
+            raise Exception("Padding must be an int or tuple of length 4, not " + str(type(float)))
+
+        # Defaults
+        # Start at first option
+        self.current_option = 0
 
     def get_surface(self):
         """
         Returns a pygame surface containing the menu to be blited into another surface
         :return: Pygame Surface
         """
-        surface = pygame.Surface(self.size)
+        surface = pygame.Surface(self.size, flags=pygame.SRCALPHA)
+        surface.fill(self.background_color)
+        # Offsets arent tuples so that they can be reassigned
+        # Padding from left
+        horizontal_offset = self.padding[-1]
+        # Padding from top
+        vertical_offset = self.padding[0]
+        for option in self.options:
+            text = self.font.render(option, self.antialias, self.text_color)
+            surface.blit(text, (horizontal_offset, vertical_offset))
+
+            # get_linesize should return recommended line size, this way text won't overlap
+            vertical_offset += self.font.get_linesize()
 
         return surface
 
@@ -68,7 +111,9 @@ def main():
     """
     Contains and sets up the main event loop for pygame
     """
+    # Start up pygame
     pygame.init()
+    pygame.font.init()
     clock = pygame.time.Clock()
 
     # Also creating a tuple because some functions take that
@@ -77,6 +122,8 @@ def main():
     pygame.display.set_caption("Dental Guardians")
 
     status = GameStatus.TITLE_SCREEN
+
+    test_menu = Menu(("Start", "End"), (200, 200), pygame.font.SysFont("Arial", 17))
 
     playing = True
     while playing:
@@ -93,12 +140,16 @@ def main():
         elif status is GameStatus.BATTLE_MENU:
             pass
 
+        screen.blit(test_menu.get_surface(), (5, 5))
+
+
         # Update display
         pygame.display.flip()
 
         clock.tick(60)  # fps
 
     # Clean up and say bye
+    pygame.font.quit()
     pygame.quit()
     print("Thanks for playing!")
 
