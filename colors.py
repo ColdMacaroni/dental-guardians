bfcc24efd131eb0b8a81588ef95a95a01b2b7f8b
muffin <diegoxe7117@gmail.com>
2021-08-26 16:10:54 +1200

    run black formatter

--- colors.py
+++ colors.py
@@ -4,6 +4,7 @@
 
 from pygame import Color
 
+
 class RGBA:
     TRANSPARENT = Color(255, 255, 255, 0)
 
@@ -20,12 +21,12 @@ class RGB:
     LIGHT_BLUE = Color(0, 255, 255)
     PINK = Color(255, 0, 255)
 
+
 # these ones are out here cause theyre independent of rgb(a)
-#Color(225, 63, 240, 128)
+# Color(225, 63, 240, 128)
 MENU_BACKGROUND = RGB.WHITE
 MENU_HIGHLIGHTED = Color(240, 204, 86, 128)
 
 if __name__ == "__main__":
     # Print defined colours
     print("Hey you should import this, not just run it!")
-
