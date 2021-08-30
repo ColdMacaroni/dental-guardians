148802c5cb0a10ca9431e1ff3e1c4ae5b2e0a7d3
muffin <diegoxe7117@gmail.com>
2021-08-31 01:07:56 +1200

    rgb were all red, fixed now

--- colors.py
+++ colors.py
@@ -14,8 +14,8 @@ class RGB:
     BLACK = Color(0, 0, 0)
 
     RED = Color(255, 0, 0)
-    GREEN = Color(255, 0, 0)
-    BLUE = Color(255, 0, 0)
+    GREEN = Color(0, 255, 0)
+    BLUE = Color(0, 0, 255)
 
     YELLOW = Color(255, 255, 0)
     LIGHT_BLUE = Color(0, 255, 255)
